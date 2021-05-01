e1cd590bb6929be8afe6a6120e714e3e7d709abb
junmt <mjsz.1206@gmail.com>
2021-05-02 01:02:01 +0900

    動画調整css追加

--- src/templates/post.js
+++ src/templates/post.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import { graphql, Link } from 'gatsby'
 import Layout from '../components/Layout'
+import '../components/youtube.css'
 
 export const BlogPostTemplate = ({
   content,
